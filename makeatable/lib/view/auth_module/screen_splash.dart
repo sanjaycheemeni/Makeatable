c7eb2e6d473f2e55bfb079c9844606f63e6dc28b
Navaneeth M <nav@Navaneeths-MacBook-Air.local>
2023-08-10 14:41:15 +0530

    auth

---
+++ makeatable/lib/view/auth_module/screen_splash.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(width: MediaQuery.of(context).size.width,
+        color: Color(0xfff1f1f1),
+        height: MediaQuery.of(context).size.height,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [Image.asset("images/logo.png",width: 150,height: 150,)],
+          ),
+        ),),
+      ));
+  }
+}

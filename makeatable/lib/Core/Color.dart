c9c50138f8fbc6a91be4377238b51fbf389d17fa
Thejus raj <thejusrajp56@gmail.com>
2023-08-10 14:40:33 +0530

    Search main page start

---
+++ makeatable/lib/Core/Color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  Color Bg = const Color(0xfffff2f2);
+  Color Brown = const Color(0xff522903);
+  Color Red = const Color(0xffE81514);
+  Color White = const Color(0xffF1F1F1);
+  Color Black = const Color(0xff1F1F1F);
+  Color Yellow = const Color(0xffFFEF62);
+}
--- makeatable/lib/view/Search module/Search_discover.dart
+++ makeatable/lib/view/Search module/Search_discover.dart
@@ -1,12 +1,79 @@
 import 'package:flutter/material.dart';
 
-class Search extends StatelessWidget {
-  const Search({super.key});
+
+class mainsearchpage extends StatefulWidget {
+  const mainsearchpage({super.key});
 
   @override
+  State<mainsearchpage> createState() => _mainsearchpageState();
+}
+
+class _mainsearchpageState extends State<mainsearchpage> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(child: Text('Search Page')),
+    return MaterialApp(
+      theme: ThemeData(fontFamily: "Poppins"),
+      home: Scaffold(
+        body: Column(
+          children: [
+            Container(
+              child: Container(
+                margin: EdgeInsets.only(top: 45, bottom: 15),
+                padding: EdgeInsets.only(right: 35,left: 23),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        Text("Discover" , style: TextStyle(fontWeight: FontWeight.bold,fontSize: 21,),
+                        ),
+                      ],
+                    ),
+                    Center(
+                      child: Container(
+                        child: Icon(Icons.filter_alt, color: Color(0xff522903)),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.transparent,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              width: 350,
+              height: 60,
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'search',
+                    suffixIcon: const Icon(Icons.search),
+                    suffixIconColor: Color(0xff522903),
+                ),
+              ),
+            ),
+            Container(
+              child: Container(
+                margin: EdgeInsets.only(top:230, bottom: 20),
+                padding: EdgeInsets.only(right: 20,left: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        Text("Recent" , style: TextStyle(fontWeight: FontWeight.bold,fontSize: 21,),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }

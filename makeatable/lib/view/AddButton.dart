f9d6c6a458db2353e089e25592a35397ea547e21
UV VAISHNAV <vyshnavpramod10@gmail.com>
2023-08-15 22:45:35 +0530

    Admin Manager Home Page

---
+++ makeatable/lib/view/AddButton.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:makeatable/util/constants/color_constants.dart';
+
+class Addbutton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:[
+              SizedBox(height: 90),
+              SvgPicture.asset('icons/image.svg',
+                  height: 150, width: 150, color: Colors.black),
+              SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                child: TextField(
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(width: 1, color: Colors.black),
+                        borderRadius: BorderRadius.circular(10)),
+                    labelText: 'Name',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                child: TextField(
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(width: 1, color: Colors.black),
+                        borderRadius: BorderRadius.circular(10)),
+                    labelText: 'Price',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                child: SizedBox(
+                  height: 90,
+                  child: TextField(
+                    maxLines: null,
+                    expands: true,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(width: 1, color: Colors.black),
+                          borderRadius: BorderRadius.circular(10)),
+                      labelText: 'Description',
+                    ),
+                  ),
+                ),
+              ),
+
+              ElevatedButton(
+                  onPressed: () {},
+                  child: Text('ADD'),
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.red,
+                      onPrimary: Colors.white,
+                      minimumSize: Size(200, 50),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15)))),
+            ],
+          ),
+        ),
+        bottomNavigationBar: bottomBar(0),
+      ),
+    );
+  }
+}
+
+Widget bottomBar(index) {
+  return BottomNavigationBar(
+    showSelectedLabels: false,
+    showUnselectedLabels: false,
+    backgroundColor: mYellow,
+    selectedItemColor: mYellow,
+    items: <BottomNavigationBarItem>[
+      BottomNavigationBarItem(
+        icon: SvgPicture.asset(
+          'icons/home.svg',
+          color: mIconInactive,
+        ),
+        label: '',
+      ),
+      BottomNavigationBarItem(
+        icon: SvgPicture.asset(
+          'icons/plus-circle.svg',
+          color: mIconActive,
+        ),
+        label: '',
+      ),
+      BottomNavigationBarItem(
+        icon: SvgPicture.asset(
+          'icons/clock.svg',
+          color: mIconInactive,
+        ),
+        label: '',
+      ),
+      BottomNavigationBarItem(
+        icon: SvgPicture.asset(
+          'icons/user.svg',
+          color: mIconInactive,
+        ),
+        label: '',
+      ),
+    ],
+    unselectedItemColor: mYellow,
+    currentIndex: index,
+  );
+}
---
+++ makeatable/lib/view/shared/Widget.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../util/constants/color_constants.dart';
+
+//search
+
+class searchbar extends StatelessWidget {
+  final controller;
+  final String hintText;
+  final bool obscureText;
+
+  const searchbar({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(20, 50, 20, 10),
+      child: TextField(
+        controller: controller,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: BorderSide(color: Color(0xFFFCDFDF)),
+
+            ),
+           /* prefixIcon: Align(
+              widthFactor: 1.0,
+              heightFactor: 1.0,
+              child: Icon(
+              Icons.arrow_back_ios,
+              ),
+            ),*/
+            suffixIcon:Icon(
+              Icons.search,
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey.shade400),
+            ),
+            fillColor: Colors.grey.shade200,
+            filled: true,
+            hintText: hintText,
+            hintStyle: TextStyle(color: Colors.grey[500])),
+      ),
+    );
+  }
+}
+//
+
+
+//top rated card
+class TopRatedCard extends StatelessWidget {
+  final String foodName, price, imageLocation;
+  const TopRatedCard(
+      {required this.foodName,
+        required this.price,
+        required this.imageLocation,
+        super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 10, right: 10),
+      child: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage(imageLocation),
+                fit: BoxFit.cover),
+            color: Color(0xaa000000),
+            borderRadius: BorderRadius.circular(12)),
+        width: 175,
+        height: 185,
+        child: Stack(
+          children: [
+            Padding(
+              padding: EdgeInsets.fromLTRB(15, 115, 0, 5),
+              child: Text(
+                foodName,
+                style: TextStyle(
+                    color: Colors.amber,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600),
+              ),
+            ),
+
+            Padding(
+              padding: EdgeInsets.fromLTRB(15, 140, 0, 15),
+              child: Text(
+                price,
+                style: TextStyle(
+                    color: Color.fromARGB(172, 255, 193, 7),
+                    fontSize: 17,
+                    fontWeight: FontWeight.w100),
+              ),
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
+
+

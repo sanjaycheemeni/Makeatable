94c6839a8b69afab4bd61b9e38eab728a4405014
Aashish <aashishanil530@gmail.com>
2023-10-14 11:32:47 +0530

    base url change

--- makeatable/lib/services/constants.dart
+++ makeatable/lib/services/constants.dart
@@ -1,5 +1,5 @@
 final base_url =
-    "http://d171-2401-4900-1c28-6c37-56fc-a13f-62fb-342f.ngrok-free.app";
+    "https://3634-2401-4900-1c28-6c37-b901-a07a-3fa9-7e2b.ngrok-free.app/";
 
 final url = {
   'USER': '$base_url/auth/register',

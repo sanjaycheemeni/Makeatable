886459077af9cd2d029a080d99bd9c26e7c33d77
Aashish <aashishanil530@gmail.com>
2023-10-18 20:54:32 +0530

    ngrok

--- makeatable/lib/services/constants.dart
+++ makeatable/lib/services/constants.dart
@@ -1,4 +1,5 @@
-final base_url = "https://f909-223-178-84-218.ngrok-free.app";
+final base_url =
+    "https://7f75-2405-201-f00d-3058-8c74-d7ba-200c-b84.ngrok-free.app";
 final url = {
   'USER': '$base_url/auth/register',
   'AUTH': '$base_url/auth/authenticate',
